67126cf868798a9dfb5b1851edaa90a0dec79321
pascal <github@picibi.fr>
2025-12-23 20:39:11 +0100

    feat: update build task
    - Updated the build task to improve readability and maintainability.
    - Added new functionality to PicoPage, including computation result handling and device status checking.
    - Introduced JSON serialization context for hybrid web view communication.

--- VopPico.App/Pages/PicoPage.xaml.cs
+++ VopPico.App/Pages/PicoPage.xaml.cs
@@ -1,7 +1,12 @@
+using System.Text.Json.Serialization;
+using Windows.ApplicationModel.VoiceCommands;
+
 namespace VopPico.App.Pages;
 
 public partial class PicoPage : ContentPage
 {
+    private int count = 0;
+
     public PicoPage()
     {
         InitializeComponent();
@@ -12,6 +17,23 @@ public partial class PicoPage : ContentPage
     {
         public string? Message { get; set; }
         public int Value { get; set; }
+    }
+    
+    public class ComputationResult
+    {
+        public double result { get; set; }
+        public string? operationName { get; set; }
+    }
+
+    [JsonSourceGenerationOptions(WriteIndented = true)]
+    [JsonSerializable(typeof(ComputationResult))]
+    [JsonSerializable(typeof(double))]
+    [JsonSerializable(typeof(string))]
+    [JsonSerializable(typeof(Dictionary<string, string>))]
+    internal partial class VopHybridJSContext : JsonSerializerContext
+    {
+        // This type's attributes specify JSON serialization info to preserve type structure
+        // for trimmed builds.    
     }
     
     private async void hybridWebView_RawMessageReceived(object sender, HybridWebViewRawMessageReceivedEventArgs e)
@@ -45,9 +67,33 @@ public partial class PicoPage : ContentPage
         Console.WriteLine($"Received code: {code}");
     }
 
-    public string GetDeviceStatus()
+    public async Task<string> GetDeviceStatus()
     {
         // Return the device status
-        return "Device is ready 1";
+        return "Device is ready 2";
+    }
+
+    public async Task checkCS2JS()
+    {
+        try
+        {
+            await Dispatcher.DispatchAsync(async () =>
+            {
+                await hybridWebView.EvaluateJavaScriptAsync("console.warn('>> CS2JS #1')");
+                hybridWebView.SendRawMessage($">> CS2JS #2 nb={count++}");
+                await hybridWebView.EvaluateJavaScriptAsync("window.vopHost.onRawMessageReceived('>> CS2JS #3');");
+                await hybridWebView.EvaluateJavaScriptAsync("window.logMessage('>> CS2JS #4', 'warning');");
+                await hybridWebView.InvokeJavaScriptAsync<object>(
+                    "window.logMessage",
+                    null,
+                    [">> CS2JS #5", "error"],
+                    [VopHybridJSContext.Default.String, VopHybridJSContext.Default.String]
+                    );
+            });
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Error in CS2JS: {ex.Message}");
+        }
     }
 }
